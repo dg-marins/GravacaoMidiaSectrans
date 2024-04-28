79c4e991eaeb40ff7c32f71c711ea5a9ef89b336
Douglas Marins <douglasmarinssouza@gmail.com>
2024-04-28 14:52:43 -0300

    Criação de função que atualiza o select de empresas

--- page/scripts/script.js
+++ page/scripts/script.js
@@ -3,3 +3,24 @@ function selecionarEmpresa() {
     var empresaSelecionada = dropdown.options[dropdown.selectedIndex].text;
     console.log("Empresa selecionada:", empresaSelecionada);
 }
+
+
+function listarEmpresas(){
+    var listaEmpresas = [
+        {empresa: "Tijuquinha", id: 2},
+        {empresa: "Graças", id: 3},
+        {empresa: "Sãen Pena, SJC", id: 4}
+    ];
+
+    var selectEmpresas = document.getElementById("empresas");
+
+    listaEmpresas.forEach(function(empresa)
+    {
+        var option = document.createElement("option");
+        option.value = empresa.id;
+        option.text = empresa.text;
+
+        empresa.appendChild(option);
+    });
+}
+
